694c8fc5defd504c990e92968c2daa7001d02e61
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-17 13:43:00 +0500

    Add files via upload

---
+++ century.cpp
@@ -0,0 +1,21 @@
+#include<iostream> 
+using namespace std;
+main(){
+ 
+cout<<"  .....                         QQQQQQ    QQQQQ                 ...........       "<<endl;
+cout<<"  ......                       Q      Q  Q     Q                ..........        "<<endl;
+cout<<"  ......                           QQQ   Q     Q                .........         "<<endl; 
+cout<<"  ........                       QQ      Q     Q                ........          "<<endl;
+cout<<"   ........                    QQQQQQQQ   QQQQQ    th           .......           "<<endl;
+cout<<"    ........               ----------------------------         ......            "<<endl;
+cout<<"      .......                  C  E  N  T  U  R  Y              .....             "<<endl;
+cout<<"        ......             ----------------------------         ....              "<<endl;
+cout<<"          ....             QQQQQ  QQQQQ  Q      Q  QQQQQ        ...               "<<endl;
+cout<<"            --               Q    Q        Q   Q     Q          --                "<<endl;
+cout<<"            _||_             Q    QQQQ       Q       Q         _||_               "<<endl;
+cout<<"           |    |            Q    Q        Q   Q     Q        |    |              "<<endl;
+cout<<"  _________|____|_________   Q    QQQQQ   Q      Q   Q  ______|____|_________     "<<endl;
+
+
+
+}
