13b842e2f7523fbea0924493ab5739f0e446b6fe
Maryam024 <144409702+Maryam024@users.noreply.github.com>
2023-09-17 13:44:27 +0500

    Add files via upload

---
+++ foody.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main(){
+
+cout<<"                                                               "<<endl;
+cout<<"                                                               "<<endl;
+cout<<"                  *        *        *                          "<<endl;
+cout<<"                     *        *       *                        "<<endl;
+cout<<"                    *        *       *                         "<<endl;
+cout<<"                   *        *       *                          "<<endl;
+cout<<"                 *        *       *                            "<<endl;
+cout<<"                  *        *       *                           "<<endl;
+cout<<"                     *        *       *                        "<<endl;
+cout<<"       **********************************************          "<<endl;
+cout<<"        *                                          *           "<<endl;
+cout<<"           *                                     *             "<<endl;
+cout<<"              *                                *               "<<endl;
+cout<<"                 *                          *                  "<<endl;
+cout<<"                   ************************                    "<<endl;
+cout<<"                                                               "<<endl;
+cout<<"             11  WELCOME TO FOODY APPLICATION 11               "<<endl;
+
+}
